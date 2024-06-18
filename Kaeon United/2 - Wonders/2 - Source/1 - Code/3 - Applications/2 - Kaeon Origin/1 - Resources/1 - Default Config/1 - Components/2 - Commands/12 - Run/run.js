79536b9c8e39c5ae24c3571b5ceb290e5ab85226
Jesse Dinkin <kaeon.ace@gmail.com>
2024-06-18 01:50:31 -0400

    Kaeon

--- Kaeon United/2 - Wonders/2 - Source/1 - Code/3 - Applications/2 - Kaeon Origin/1 - Resources/1 - Default Config/1 - Components/2 - Commands/12 - Run/run.js
+++ Kaeon United/2 - Wonders/2 - Source/1 - Code/3 - Applications/2 - Kaeon Origin/1 - Resources/1 - Default Config/1 - Components/2 - Commands/12 - Run/run.js
@@ -1,5 +1,5 @@
 var moduleDependencies = {
-	virtualRun: "https://raw.githubusercontent.com/Kaeon-United/Kaeon-United/main/Kaeon%20United/2%20-%20Wonders/2%20-%20Source/1%20-%20Code/3%20-%20Applications/1%20-%20Applications/2%20-%20Utilities/3%20-%20Virtual%20Run/virtualRun.js"
+	virtualRun: "https://kaeon-united.github.io/?unitedjs=https://raw.githubusercontent.com/Kaeon-United/Kaeon-United/main/Kaeon%20United/2%20-%20Wonders/2%20-%20Source/1%20-%20Code/3%20-%20Applications/1%20-%20Applications/2%20-%20Utilities/3%20-%20Virtual%20Run/virtualRun.js"
 };
 
 var virtualSystem = require("kaeon-united")("virtualSystem");
@@ -18,14 +18,19 @@ mark = mark.substring(0, mark.length - 1).trim();
 let args = Array.from(arguments);
 
 let options = args.filter(arg => Object.keys(opCodes).includes(arg));
+
 let path = args.filter(arg => !Object.keys(opCodes).includes(arg))[0];
+path = virtualSystem.getAbsolutePath(path, mark);
+
+if(path.includes(":/") && !path.includes("://"))
+	path = path.split(":/").join("://");
 
 args = args.filter(arg => !Object.keys(opCodes).includes(arg)).slice(1);
 
 window.open(
 	moduleDependencies.virtualRun +
 		"&path=" +
-		encodeURIComponent(virtualSystem.getAbsolutePath(path, mark)) +
+		encodeURIComponent(path) +
 		options.map(option => opCodes[option]).join("") +
 		"&args=" +
 		encodeURIComponent(JSON.stringify(args)),
